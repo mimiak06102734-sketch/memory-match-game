5a65e273eaf810110728b182cf38ecd8939067c6
mimiak06102734-sketch <mimiak.06102734@gmail.com>
2026-09-10 17:58:54 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -6,9 +6,9 @@ const ASSETS = [
   "./style.css",
   "./script.js",
   "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png",
-  "./icons/apple-touch-icon.png"
+  "./icon-192.png",
+  "./icon-512.png",
+  "./apple-touch-icon.png"
 ];
 
 self.addEventListener("install", event => {
